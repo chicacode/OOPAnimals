c8b3569ab81cbc0af1d55392437df05c522c8958
chicacode <gerahernan.al@gmail.com>
2022-10-13 21:51:14 -0700

    added instance of reptile

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import Animal.Animal;
+import Reptile.Reptile;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -7,5 +8,9 @@ public class App {
         Animal dog = new Animal(3, 2, "Dog", "Warm");
 
         System.out.println("The man's best friend is: " + dog.getAnimalType());
+
+        Reptile snake = new Reptile("dry", null, null, 0, 0, "snake", null);
+
+        System.out.println("The " + snake.getAnimalType() + " is in the grass" + " and its skin is " + snake.getSkinType());
     }
 }
